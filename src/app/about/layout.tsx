5a0eb8278a7ce4a761f82d6f3dda6209a175d150
RioRivaldi <rivaldirio090@gmail.com>
2024-07-10 11:11:55 +0700

    update

---
+++ src/app/about/layout.tsx
@@ -0,0 +1,13 @@
+export default function AboutLayout () {
+    return (
+      <>
+        <nav className="fixed left-0 top-10 z-10 h-screen w-60 bg-gray-800">
+          <ul className="text-white p-5">
+            <li>Home</li>
+            <li>About</li>
+            <li>Profile</li>
+          </ul>
+        </nav>
+      </>
+    );
+}
